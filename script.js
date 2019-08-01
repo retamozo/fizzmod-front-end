a0b1b7e317ec159f3ebbe389aeaf37bed1ff2c2a
Horacio Gutierrez <horacio.estevez@gmail.com>
2019-07-31 21:51:38 -0300

    Agrego refe de fragmentos, appendChild, y eventos

--- script.js
+++ script.js
@@ -178,3 +178,33 @@ document.body.addEventListener("click",e=>{
        console.log("Click del boton nuevo")
    }
 })
+
+
+/**
+ * 
+ * form : submit
+ * 
+ * a : click
+ * 
+ */
+
+var a = document.querySelector("a")
+a.addEventListener("click", e => {
+    e.preventDefault();
+    //Usamos prevent default porque hay algunos elementos en la sesion que vienen con comportamientos predefinidos y en algunos casos no queremos que eso pase, entonces para cancelar eso, desde el objeto Event ejecutamos su metodo Event.preventDefault()
+    console.log("Click")
+})
+
+/**
+ * Subir todo el trabajo en un branch nuevo y pushearlo a un repo en GitHub 
+ * 
+ * 1) Hacer una etiqueta <a> en HTMl
+ * 2) Agarrarla con JS
+ * 3) Asignarle un evento de "click"
+ * 4) El callback del click tiene que poder crear un elemento <p> y dos <button>, todo adentro de un <div> (Se crean con JS)
+ * 5) El <p> tiene que decir "Esta seguro que quiere abandonar la página" y los botones "aceptar" y "cancelar", respectivamente
+ * 6) Si se hace click en aceptar , se tiene que redirigir al usuario al href del <a>
+ * 7) Si se hace click en cancelar, se tiene que borrar el mensaje 
+ * 
+ * 
+ */
